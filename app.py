97826344c3f83c29c87b5764ce6935e57a3d0488
singsingseaweed <135815622+singsingseaweed@users.noreply.github.com>
2024-04-08 00:49:14 +0900

    app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import math
 
-def distribute_money(hours_per_group, total_income):
-    total_hours_worked = sum(hours_per_group)
-    total_people = sum(len(group) for group in hours_per_group)
+def distribute_money(groups_info, total_income):
+    total_hours_worked = sum(group['hours'] for group in groups_info)
+    total_people = sum(group['num_people'] for group in groups_info)
     
     # 각 그룹의 시간에 따른 분배 비율 계산
-    distribution_ratio = [sum(group) / total_hours_worked for group in hours_per_group]
+    distribution_ratio = [group['hours'] / total_hours_worked for group in groups_info]
     
     # 각 그룹의 분배 금액 계산
     group_incomes = [ratio * total_income for ratio in distribution_ratio]
@@ -25,10 +25,11 @@ st.title("돈을 공정하게 분배해주는 앱")
 # 각 그룹의 정보 입력 받기
 num_groups = st.number_input("그룹의 수를 입력하세요", min_value=1, step=1, value=1)
 
-hours_per_group = []
+groups_info = []
 for i in range(num_groups):
-    group_hours = st.number_input(f"{i+1}번째 그룹의 일한 시간을 입력하세요", min_value=0, step=1)
-    hours_per_group.append([group_hours])
+    group_hours = st.number_input(f"{i+1}번째 그룹의 총 일한 시간을 입력하세요", min_value=0, step=1)
+    group_num_people = st.number_input(f"{i+1}번째 그룹의 인원 수를 입력하세요", min_value=1, step=1)
+    groups_info.append({'hours': group_hours, 'num_people': group_num_people})
 
 # 전체 수익 입력 받기
 total_income = st.number_input("전체 수익을 입력하세요", min_value=0)
@@ -36,7 +37,7 @@ total_income = st.number_input("전체 수익을 입력하세요", min_value=0)
 # "분배하기" 버튼 클릭 시 실행되는 코드
 if st.button("분배하기"):
     # 분배된 돈 계산
-    group_incomes, remaining_change = distribute_money(hours_per_group, total_income)
+    group_incomes, remaining_change = distribute_money(groups_info, total_income)
 
     # 결과 출력
     st.write("그룹별 분배된 돈:")
