cbefb8600ce86aa55e222ffbec70a722c4c58d5a
singsingseaweed <135815622+singsingseaweed@users.noreply.github.com>
2024-04-08 00:37:18 +0900

    app.py

--- app.py
+++ app.py
@@ -22,15 +22,32 @@ def distribute_money(hours_worked, total_income):
 # Streamlit 애플리케이션 제목 설정
 st.title("돈을 공정하게 분배해주는 앱")
 
-# 각 개인의 정보 입력 받기
-num_people = st.number_input("일한 사람의 수를 입력하세요", min_value=1, step=1, value=1)
+# 최대 일한 시간 설정
+max_hours_worked = st.number_input("모든 사람이 최대로 일한 시간을 입력하세요", min_value=1, step=1, value=8)
 
-hours_worked = []
-for i in range(num_people):
-    hours_worked.append(st.number_input(f"{i+1}번째 사람의 일한 시간을 입력하세요", min_value=0, step=1))
+# 먼저 집에간 그룹의 정보 입력 받기
+num_group1 = st.number_input("먼저 집에 간 그룹의 사람 수를 입력하세요", min_value=0, step=1, value=0)
+group1_hours_worked = st.number_input("먼저 집에 간 그룹의 총 일한 시간을 입력하세요", min_value=0, step=1, value=0)
+
+# 두 번째로 집에간 그룹의 정보 입력 받기
+num_group2 = st.number_input("그 다음 집에 간 그룹의 사람 수를 입력하세요", min_value=0, step=1, value=0)
+group2_hours_worked = st.number_input("그 다음 집에 간 그룹의 총 일한 시간을 입력하세요", min_value=0, step=1, value=0)
+
+# 세 번째로 집에간 그룹의 정보 입력 받기
+num_group3 = st.number_input("마지막으로 집에 간 그룹의 사람 수를 입력하세요", min_value=0, step=1, value=0)
+group3_hours_worked = st.number_input("마지막으로 집에 간 그룹의 총 일한 시간을 입력하세요", min_value=0, step=1, value=0)
 
 # 전체 수익 입력 받기
 total_income = st.number_input("전체 수익을 입력하세요", min_value=0)
+
+# 각 그룹의 총 일한 시간 계산
+total_group_hours_worked = group1_hours_worked + group2_hours_worked + group3_hours_worked
+
+# 모든 사람들의 총 일한 시간 계산
+total_hours_worked = max(max_hours_worked, total_group_hours_worked)
+
+# 모든 그룹에 속한 사람들의 일한 시간 계산
+hours_worked = [total_hours_worked] * num_group1 + [total_hours_worked] * num_group2 + [total_hours_worked] * num_group3
 
 # "분배하기" 버튼 클릭 시 실행되는 코드
 if st.button("분배하기"):
