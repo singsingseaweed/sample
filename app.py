7c71e588c4ee45292f6bddecc74ff184e81dad09
singsingseaweed <135815622+singsingseaweed@users.noreply.github.com>
2024-04-08 00:02:11 +0900

    app.py

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import streamlit as st
 import pandas as pd
+import os
+
+# 데이터 파일의 절대 경로
+data_file_path = os.path.abspath("data.csv")
 
 # 데이터 가져오기
-df = pd.read_csv("data.csv")
+df = pd.read_csv(data_file_path)
 
 # 웹 애플리케이션 정의
 st.title("간단한 데이터 시각화 앱")
