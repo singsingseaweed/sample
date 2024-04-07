8b8d7ad01044b5830be87f04ba61ad1cf57a0208
singsingseaweed <135815622+singsingseaweed@users.noreply.github.com>
2024-04-07 23:43:05 +0900

    app.py

--- app.py
+++ app.py
@@ -1,27 +1,38 @@
 import streamlit as st
+
+# 타이틀 추가
+st.title('나의 첫 Streamlit 앱')
+
+# 텍스트 추가
+st.write('안녕하세요! 이것은 Streamlit으로 만든 첫 웹 페이지입니다.')
+
+# 그래프 추가
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.signal as signal
 
-st.set_page_config(
-    page_title="제어공학기말",
-)
+data = np.random.randn(100)
+plt.hist(data, bins=20)
+st.pyplot()
 
-st.header("응답곡선")
+# 사이드바에 입력 필드 추가
+name = st.sidebar.text_input('이름 입력', '이름을 입력하세요')
+st.sidebar.write('안녕하세요,', name, '님!')
 
-num = [100]
-den = [1, 5, 106]
+# 다양한 위젯 추가
+number = st.number_input('숫자 입력', value=10)
+st.write('입력된 숫자는', number, '입니다.')
 
-ltf = signal.TransferFunction(num, den)
+# 버튼 추가
+if st.button('버튼을 클릭하세요'):
+    st.write('버튼이 클릭되었습니다!')
 
-t = np.linspace(0, 5, 1000)
-u = np.ones_like(t)
+# 파일 업로드 기능 추가
+uploaded_file = st.file_uploader('파일 업로드', type=['txt', 'csv'])
+if uploaded_file is not None:
+    st.write('파일이 성공적으로 업로드 되었습니다.')
 
-t, y, _ = signal.step(ltf, T=t)
+# 외부 링크 추가
+st.write('OpenAI의 웹사이트를 방문해보세요:', '[OpenAI 웹사이트](https://openai.com)')
 
-fig, ax = plt.subplots()
-ax.plot(t, y)
-ax.set(xlabel='Time', ylabel='Output', title='Step Response')
-ax.grid(True)
-
-st.pyplot(fig)
+# 맵 추가
+st.map()
