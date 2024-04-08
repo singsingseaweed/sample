563a978b55f5bf02bef278f527635191f58a742a
singsingseaweed <135815622+singsingseaweed@users.noreply.github.com>
2024-04-08 21:37:20 +0900

    app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import streamlit as st
 import math
 
 # 시간에 따라 분배하는 함수
-def distribute_money(hours_worked, total_income, num_dropouts, dropout_hours):
+def distribute_money(hours_worked, total_income):
     total_hours_worked = sum(hours_worked)
-    total_people = len(hours_worked) - num_dropouts
+    total_people = len(hours_worked)
     
     # 각 개인의 분배 비율 계산
     distribution_ratio = [hours / total_hours_worked for hours in hours_worked]
@@ -15,11 +15,8 @@ def distribute_money(hours_worked, total_income, num_dropouts, dropout_hours):
     # 최소 금액을 각 개인에게 적용
     individual_incomes = [min_amount_per_person] * total_people
     
-    # 중간 이탈자들의 금액 계산
-    dropout_income = min_amount_per_person * num_dropouts
-    
     # 남은 금액 계산
-    remaining_amount = total_income - min_amount_per_person * total_people - dropout_income
+    remaining_amount = total_income - min_amount_per_person * total_people
     
     # 남은 금액을 시간당 비율에 따라 배분
     for i in range(total_people):
@@ -29,7 +26,7 @@ def distribute_money(hours_worked, total_income, num_dropouts, dropout_hours):
     rounded_incomes = [math.floor(income / 100) * 100 for income in individual_incomes]
     
     # 남은 잔돈 계산
-    remaining_change = total_income - sum(rounded_incomes) - dropout_income
+    remaining_change = total_income - sum(rounded_incomes)
     
     return rounded_incomes, remaining_change
 
@@ -38,16 +35,10 @@ st.title("시간으로 금액분배")
 
 # 각 개인의 정보 입력 받기
 num_people = st.number_input("전체 참여인원", min_value=1, step=1, value=1)
-num_dropouts = st.number_input("중간 이탈자 수", min_value=0, step=1, value=0)
-
 hours_worked = []
-dropout_hours = []
 
 for i in range(num_people):
-    if i < num_dropouts:
-        dropout_hours.append(st.number_input(f"{i+1}번째 중간 이탈자의 회식한 시간", min_value=0, step=1))
-    else:
-        hours_worked.append(st.number_input(f"{i+1}번째 참여인의 회식한 시간", min_value=0, step=1))
+    hours_worked.append(st.number_input(f"{i+1}번째 참여인의 회식한 시간", min_value=0, step=1))
 
 # 전체 수익 입력 받기
 total_income = st.number_input("총 금액", min_value=0)
@@ -55,11 +46,11 @@ total_income = st.number_input("총 금액", min_value=0)
 # "분배하기" 버튼 클릭 시 실행되는 코드
 if st.button("분배하기"):
     # 유효성 검사
-    if total_income <= 0 or any(hour < 0 for hour in hours_worked) or any(hour < 0 for hour in dropout_hours):
+    if total_income <= 0 or any(hour < 0 for hour in hours_worked):
         st.error("잘못된 입력입니다.")
     else:
         # 각 개인이 받아야 할 돈 계산
-        individual_incomes, remaining_change = distribute_money(hours_worked, total_income, num_dropouts, dropout_hours)
+        individual_incomes, remaining_change = distribute_money(hours_worked, total_income)
 
         # 결과 출력
         for i, income in enumerate(individual_incomes):
