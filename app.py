f372fa16d287ecc6767a5b9a162ace489ef5c931
singsingseaweed <135815622+singsingseaweed@users.noreply.github.com>
2024-04-08 20:42:37 +0900

    app.py

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ import streamlit as st
 import math
 
 # 시간에 따라 분배하는 함수
-def distribute_money(common_hours, less_hours, total_income):
-    total_people = len(less_hours) + 1  # 공통 시간에 근무하는 사람들과 덜 근무하는 사람의 수
+def distribute_money(common_hours, less_hours, total_income, total_people):
+    # 공통 시간에 근무하는 사람들과 덜 근무하는 사람의 수
+    num_people_with_less_hours = len(less_hours)
+    total_people = total_people + num_people_with_less_hours
     
     # 덜 근무하는 사람들의 비율 계산
     distribution_ratio = [hours / sum(less_hours) for hours in less_hours]
@@ -19,7 +21,7 @@ def distribute_money(common_hours, less_hours, total_income):
     
     # 남은 금액을 시간당 비율에 따라 배분
     for i in range(len(less_hours)):
-        individual_incomes[i + 1] += remaining_amount * distribution_ratio[i]
+        individual_incomes[i + num_people_with_less_hours] += remaining_amount * distribution_ratio[i]
     
     # 각 개인에게 분배된 돈을 100단위로 떨어지도록 조정
     rounded_incomes = [math.floor(income / 100) * 100 for income in individual_incomes]
@@ -45,6 +47,9 @@ for i in range(num_people_with_less_hours):
 # 전체 수익 입력 받기
 total_income = st.number_input("총 금액", min_value=0)
 
+# 전체 인원 입력 받기
+total_people = st.number_input("전체 인원", min_value=1, step=1)
+
 # "분배하기" 버튼 클릭 시 실행되는 코드
 if st.button("분배하기"):
     # 유효성 검사
@@ -52,12 +57,12 @@ if st.button("분배하기"):
         st.error("잘못된 입력입니다.")
     else:
         # 각 개인이 받아야 할 돈 계산
-        individual_incomes, remaining_change = distribute_money(common_hours, less_hours, total_income)
+        individual_incomes, remaining_change = distribute_money(common_hours, less_hours, total_income, total_people)
 
         # 결과 출력
-        st.write("공통된 시간을 근무하는 사람: ", f"{total_income / (2 * len(less_hours) + 1):,.0f}원")
-        for i, income in enumerate(individual_incomes[1:], start=1):
-            st.write(f"{i}번째 원래보다 덜 근무하는 사람: {income:,.0f}원")
+        st.write("공통된 시간을 근무하는 사람: ", f"{total_income / (2 * total_people):,.0f}원")
+        for i, income in enumerate(individual_incomes[num_people_with_less_hours:], start=num_people_with_less_hours):
+            st.write(f"{i+1}번째 원래보다 덜 근무하는 사람: {income:,.0f}원")
 
         # 잔돈 출력
         if remaining_change > 0:
