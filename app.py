0934223e6f0ae31d0323653f89392349f667ae13
singsingseaweed <135815622+singsingseaweed@users.noreply.github.com>
2024-04-08 22:28:49 +0900

    app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def distribute_money(hours_worked, total_income):
     distribution_ratio = [hours / total_hours_worked for hours in hours_worked]
     
     # 최소 금액 계산
-    min_amount_per_person = (total_income * 3) / (5 * total_people)
+    min_amount_per_person = (total_income * 11) / (7 * total_people)
     
     # 최소 금액을 각 개인에게 적용
     individual_incomes = [min_amount_per_person] * total_people
@@ -31,21 +31,21 @@ def distribute_money(hours_worked, total_income):
     return rounded_incomes, remaining_change
 
 # Streamlit 애플리케이션 제목 설정
-st.title("시간으로 금액분배")
+st.title("회식비 분배")
 
 # 전체 참여인원 입력 받기
 num_people = st.number_input("전체 참여인원", min_value=1, step=1, value=1)
 
 # 이탈하지 않은 사람들의 회식시간 입력 받기
-default_hours = st.number_input("이탈하지 않은 사람들의 회식한 시간", min_value=0, step=1)
+default_hours = st.number_input("전체 회식시간", min_value=0, step=1)
 
 # 중간에 이탈한 사람의 수 입력 받기
-num_quitters = st.number_input("중간에 이탈한 사람의 수", min_value=0, max_value=num_people-1, step=1, value=0)
+num_quitters = st.number_input("중간 귀가자 수", min_value=0, max_value=num_people-1, step=1, value=0)
 
 # 이탈한 사람들의 회식시간 입력 받기
 quitter_hours = []
 for i in range(num_quitters):
-    quitter_hours.append(st.number_input(f"{i+1}번째 이탈한 사람의 회식한 시간", min_value=0, step=1))
+    quitter_hours.append(st.number_input(f"{i+1}번째 귀가한 사람의 회식시간", min_value=0, step=1))
 
 # 전체 수익 입력 받기
 total_income = st.number_input("총 금액", min_value=0)
@@ -75,7 +75,7 @@ if st.button("분배하기"):
             total_amount = sum(participant[1] for participant in participants)
             per_person_amount = total_amount / len(participants)
             num_participants = len(participants)
-            st.write(f"{hours}시간 참가자들의 금액: {per_person_amount:,.0f}원 ({num_participants}명)")
+            st.write(f"{hours}시간 참가자들: {per_person_amount:,.0f}원 ({num_participants}명)")
 
         # 잔돈 출력
         if remaining_change > 0:
